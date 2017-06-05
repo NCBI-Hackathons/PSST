537081c058cf0812edd5a8697b6c305110ed2d40
chipmash <chipmash@gmail.com>
2017-06-05 17:28:04 -0400

    Removed column with counts from top 100 disease phenotype output file

--- get_disease_phenotypes.sh
+++ get_disease_phenotypes.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 awk '{if ($0 ~ /CLNSIG=5/) print $0}' GRCh38_latest_clinvar.vcf| egrep -o 'CLNDBN[^:]+' | awk -F";" '{print $1}' | sort | uniq -c | sort -nr | grep -v "not_provided" | tr '|' '\n' | tr '\' '\n' | sed 's/CLNDBN[=]//g' | sort -nr  | head -100 > top100_disease_phenotypes
+
+awk '{$1=""}1' top100_disease_phenotypes | awk '{$1=$1}1' | sort | uniq > temp_file; mv temp_file top100_disease_phenotypes
