43bede681637a3ddd6d1bd2a68d8a540d1cbc58b
chipmash <chipo.mashayamombe@postgrad.manchester.ac.uk>
2017-06-06 15:42:32 -0400

    Updated summary_table.sh
    Added variables to store the data for the disease phenotype as well as the
    number of pathogenic variants associated with the phenotype (PATHOGENIC), snps associated with the phenotype (SNPs), SRA datasets associated with the disease phenotype (SRA), GEO datasets associated with the disease phenotype (GEO)

--- summary_table.sh
+++ summary_table.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-
+ 
+# create variables to store the data for the disease phenotype as well as the
+# number of 
+#   -pathogenic variants associated with the phenotype (PATHOGENIC)
+#   -snps associated with the phenotype (SNPs)
+#   -SRA datasets associated with the disease phenotype (SRA)
+#   -GEO datasets associated with the disease phenotype (GEO)
 
 DISEASE=""
-PATHOGENIC=""	
+PATHOGENIC=""  
 SNPs=""
 SRA=""
 GEO=""
+ 
+# Assign the top 100 disease phenotypes to a variable
+top100=$(cat top100_disease_phenotypes)
+ 
+for PHENOTYPE in $top100
+do
+    DISEASE="${DISEASE} $PHENOTYPE"
+    PATHOGENIC="${PATHOGENIC} $(esearch -query $PHENOTYPE -db clinvar|efilter -query "pathogenic" | xtract -pattern ENTREZ_DIRECT -element Count)"
+    SNPs="${SNPs} $(esearch -query $PHENOTYPE -db pubmed | elink -target snp | esummary | xtract -pattern DocumentSummary -element SNP_ID | wc -l)"
+    SRA="${SRA} $(esearch -query $PHENOTYPE -db sra |xtract -pattern ENTREZ_DIRECT -element Count)"
+    GEO="${GEO} $(esearch -query $PHENOTYPE -db gds |xtract -pattern ENTREZ_DIRECT -element Count)"
+done
 
 
