2d3660964a468c9ca9bfa6eed0341a2cfd58845e
Sean La <seanla1996@gmail.com>
2017-06-22 15:44:43 -0700

    renamed a python file

--- findFlankingSequences.py
+++ findFlankingSequences.py
@@ -24,7 +24,7 @@ def get_major_allele(seq):
 	major_allele = seq[4:left_bracket_index] + major_var + seq[right_bracket_index + 1:]
 	return major_allele
 
-def write_fasta(variants,temp):
+def write_fasta(variants,output):
 	'''
 	Given known variants, writes a FASTA file in temp
 	Inputs
@@ -33,43 +33,31 @@ def write_fasta(variants,temp):
 	Returns
 	- (dict) paths: paths to the FASTA files
 	'''
-	paths = {}
-	for varId in variants:
-		fasta_path = "%s/%s.fasta" % (temp,varId)
-		with open(fasta_path,'w') as fasta:
+	with open(output,'w') as fasta:
+	# Add the flanking sequence for each SNP to the FASTA file
+		for varId in variants:
 			fasta.write( ">%s\n" % (varId) )
 			fasta.write( "%s\n" % (variants[varId]) ) 
-		paths[varId] = fasta_path
-	return paths
-
-def make_magicblastdb(fasta_paths,temp):
-	'''
-	Given a dictionary of RNAseq variant FASTA files, creates magicblast databases
-	Inputs
-	- (dict) fasta_paths: dictionary containing paths to fasta files
-	- (str) temp: directory to contain magicblast db files	
-	'''
-	for varId in fasta_paths:
-		fasta_path = fasta_paths[varId]
-		command = "makeblastdb -dbtype nucl -in %s -out %s/%s" % (fasta_path,temp,varId)
-		subprocess.Popen(command)
 
 # Arguments parser
 parser = argparse.ArgumentParser(add_help=False,description=
 		'''
-		Author: Sean La. Given the SEQ ID of a known variant in RNA-seq data, does stuff with it.
+		Given a phenotype (e.g. "breast cancer"), returns the flanking sequences of all SNPs associated \
+		with the input phenotype.
 		''')
 parser.add_argument('email',type=str,help='Email to make use of Entrez')
-parser.add_argument('id',type=str,help='ID of the variant')
-parser.add_argument('temp',type=str,help='Directory to hold temporary files')
+parser.add_argument('phenotype',type=str,help='Phenotype')
+parser.add_argument('output',type=str,help='Output')
 parser.add_argument('-h','--help',action='help',default=argparse.SUPPRESS,help='Show this help message and exit.')
 args = parser.parse_args()
 
+# DEPRECRATED: do not use the code below for production.
+'''
 # Always tell NCBI who you are
 Entrez.email = args.email
 # Find the data from the SNP database
 Entrez.tool = "Polygenic Variant Checker"
-search_handle = Entrez.esearch(db="snp",term=args.id)
+search_handle = Entrez.esearch(db="snp",term=args.output)
 search_record = Entrez.read(search_handle)
 # For now, just take the first item in the IdList. We might use all of them in the future.
 summary_handle = Entrez.esummary(db="snp",id=search_record["IdList"][0],retmax=search_record["RetMax"])
@@ -86,7 +74,6 @@ seq = seq[0]
 major_allele = get_major_allele(seq)
 # Combine all this information into one neat package
 variants = { args.id : major_allele }
-# Create FASTA files for the variant
+# Create FASTA file for the flanking sequences of the SNPs
 paths = write_fasta(variants, args.temp)
-# Construct a magicblast database out of the variants
-make_magicblastdb(paths,args.temp)
+'''
