a03f88718aa89df3bc0be7f0193a91920f71f5bd
Sean La <seanla1996@gmail.com>
2017-07-07 21:07:10 -0700

    finished implementing variant calling script

--- callVariants.py
+++ callVariants.py
@@ -1,34 +1,133 @@
 #!/usr/bin/env python
-import queriesWithVar
+import argparse
+import os
+from __future__ import division # This modifies Python 2.7 so that any expression of the form int/int returns a float 
 
-def sra_contains_variant(mbo_path):
+# Global variables with accessing elements within tuples
+ACC = 0
+PATH = 1
+
+def get_mbo_paths(directory):
 	'''
-	Determines whether a variant is contained in an SRA dataset given a Magic-BLAST tabulated output file.
+	Given a directory, retrieves the paths of all files within the directory whose extension is .mbo
 	Inputs
-	- (str) mbo_path: path to a Magic-BLAST output file in tabulated format
+	- (str) directory: directory to be scoured for mbo files
 	Outputs
-	- True if the SRA dataset contains the variant, false otherwise
+	- paths: a dictionary where the keys are accessions and the values are paths
 	'''
+	paths = {}
+	for file in os.listdir(directory):
+		if file.endswith(".mbo"):
+			accession = os.path.basename(file)	
+			path = os.path.join(directory,file)
+			paths[accession] = path
+	return paths
 
-
-def call_variants(directory,sra_accessions,var_accessions):
+def get_btops(paths):
 	'''
-	Given a list of paths to Magic-BLAST output files in tabulated format, determines which contain the variant
-	in question 
+	Given a list of paths as described in the function get_mbo_paths, retrieves the BTOP string for each
+	alignment.
 	Inputs
-	- (list of str) mbo_list: contains the paths of all the Magic-BLAST output list
-	- (list of str) sra_accessions: contains the SRA accessions in consideration
-	- (list of str) var_accessions: contains the variant accessions in consideration
+	- paths: a list of pairs where the first entry of the pair is the accession and the second is the path
 	Outputs
-	- (dict of list) variants_in_sra: keys are SRA accessions, output is a list which contains which variants
-	  are contained in the SRA 
+	- a dictionary where keys are SRA accessions and the values are alignment dictionaries
 	'''
-	variants_in_sra = {}
-	for sra in sra_accessions:
-		for var in var_accessions:
-			path = "%s/%s.%s.mbo" 5 (directory,sra,var)	
-			if sra_contains_variant(path):
-				if sra not in variants_in_sra:
-					variants[sra] = []
-				variants[sra].append(var)
-	return variants_in_sra
+	btops = {}
+	for accession in paths:
+		path = pair[accession]
+		alignments = []	
+		with open(path,'r') as mbo:
+			for line in mbo:
+				tokens = line.split()
+				snp_acc = tokens[1]
+				ref_start = tokens[8]
+				ref_stop = tokens[9]
+				btop = tokens[16]
+				alignment = { 'snp_acc': tokens[1], 'ref_start': tokens[8],\
+					      'ref_stop': tokens[9], 'btop': tokens[16] }
+				alignments.append( alignment )
+		btops[accession] = alignments
+	return btops
+
+def get_flanks(path):
+	'''
+	Retrieves the flanking sequence lengths for the SNP sequences			
+	Inputs
+	- path: path to the file that contains the flanking sequence lengths
+	Outputs
+	- flanks: a dictionary where the keys are SNP accessions and the values are pairs where the first item
+                  is the length of the left flank and the second is the length of the right flank
+	'''
+	flanks = {}
+	with open(path,'r') as file:
+		for line in file:
+			tokens = line.split()
+			accession = tokens[0]
+			left = tokens[1]
+			right = tokens[2]
+			flanks[accession] = (left,right)	
+	return flanks
+
+def call_snps(snp_freq):
+	'''
+	Determines which snps exist in a given SRA dataset given the number of reads that do and do not contain
+	the snp
+	Inputs
+	- snp_freq: a dict where the keys are SNP accessions and the values are dicts which contain the frequency of
+		    reads that do and reads that do not contain the SNP
+	Outputs
+	- snps: a list which contains the SNP accessions of those SNPs which exist in the SRA dataset
+	'''
+	snps = []
+	for snp_acc in snp_freq:
+		frequencies = snp_freq
+		true = frequencies['true']
+		false = frequencies['false']
+		percentage = true / (true + false)
+		if percentage > 0.4: # For now, we use this simple heuristic.
+			snps.append(snp_acc)
+	return snps
+
+def get_sra_snps(btops,flanks)
+	'''
+	For all SRA accession, determines which snps exist in the SRA dataset	
+	Inputs
+	- btops: dict where the keys are SRA accessions and the values are lists of alignment dicts
+	- flanks: dict where the keys are SNP accessions and the values are the lengths of the flanks
+	Outputs
+	- snps: dict where the keys are the SRA accessions and the value is a list which contains the accessions
+		    of SNPs which exist in the SRA dataset
+	'''
+	snps = {}
+	for sra_acc in btops:
+		snp_freq = {}
+		alignments = btops[sra_acc]
+		for alignment in alignments:
+			snp_acc = alignments['snp_acc']
+			if snp_acc not in snp_freq:
+				snp_freq[snp_acc] = { 'true': 0, 'false': 0 }
+			snp_called = query_contains_ref_base(alignment)
+			if snp_called:
+				snp_freq[snp_acc]['true'] += 1	
+			else:
+				snp_freq[snp_acc]['false'] += 1	
+		sra_snps = call_snps(snp_freq) 
+		snps[sra_acc] = sra_snps	
+	return snps
+			
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('directory', help=
+	"""
+	Input directory to the Magic-BLAST output files in tabulated format.
+	""")
+parser.add_argument('flanks', help=
+	"""
+	Input file for lengths of flanks for each SNP sequence
+	""")
+args = parser.parse_args()
+
+paths = get_mbo_paths(args.directory)
+btops = get_btops(paths)
+flanks = get_flanks(args.flanks)
+snps = get_sra_snps(btops,flanks)
