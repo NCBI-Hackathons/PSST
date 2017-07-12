2581b46113f9802a03e0dc6f25c116132f6e9415
Sean La <seanla1996@gmail.com>
2017-07-12 10:40:56 -0700

    Completed implementation and testing of queries_with_ref_base.py

--- queries_with_ref_base.py
+++ queries_with_ref_base.py
@@ -45,16 +45,15 @@ def find_delimited_btop(btop):
 
 def delimited_btop_to_alignment(delimited_btop):
 	'''
-	Given a delimited BTOP string, output an alignment where matches are given as X in both the query and ref.
+	Given a delimited BTOP string, outputs the reference sequence alignment 
 	Inputs
 	- (str) delimited_btop: the space delimited BTOP string
 	Outputs
-	- str,str: first item in tuple is the reference alignment, the second is the query alignment 
+	- (str) ref: the reference sequence alignment
 	'''
 	# Construct an alignment out of the delimited BTOP string 
 	btop_list = delimited_btop.split()
 	ref = ""
-	query = ""
 	gap = False
 	for btop in btop_list:
 		if '_' in btop:
@@ -69,15 +68,34 @@ def delimited_btop_to_alignment(delimited_btop):
 			else:
 				base = MATCH
 			ref = ref + (base * num_matches)
-			query = query + (base * num_matches)
 		else:
-			query = query + btop[QUERY]
 			ref = ref + btop[REF]	
-	return ref,query
+	return ref
 
-def find_left_alignment_flank(left,ref):
+def read_flank_info(path):
 	'''
-	find the position of the base of interest in the reference alignment.
+	Given the path to a file containing the flank information for a set of variants, returns a dict with that info
+	Inputs
+	- (str) path to the file
+	Outputs
+	- a dictionary where the keys are the variant IDs and the values are a list containing the flank information
+	'''
+	flank_info = {}
+	with open(path,'r') as flanks:
+		for line in flanks:
+			tokens = line.split()
+			# The first item is the ID of the SNP
+			# The rest are the flank lengths and length of the major allele sequence
+			flank_info[tokens[0]] = {'left':tokens[1],'right':tokens[2],'length':tokens[3]}
+	return flank_info
+			
+
+def translate_var_boundary(left,ref):
+	'''
+	Translates the left boundary of the variant of interest in the reference sequence into the left boundary
+	of the variant in the reference alignment
+	e.g.
+	Given ref = ATCG, left = 1 and ref_alignment = A-TCG, this script outputs alignment_left = 2
 	Inputs
 	- (int) left: the length of the left flank in the alignment
 	- (str) ref: the reference alignment
@@ -94,61 +112,75 @@ def find_left_alignment_flank(left,ref):
 		alignment_left += 1
 	return alignment_left + 1
 
-def query_contains_ref_base(alignment,flanks):
+def query_contains_ref_base(alignment,flank_info):
 	'''
 	Determines whether the query sequence as encoded by the BTOP string contains the ref base at pos.
 	Inputs
 	- alignments: contains information regarding the magic-blast alignment for the query
-	- flanks: lengths of the SNP flanks
+	- flank_info: lengths of the SNP flanks and length of the major allele
 	Outputs
 	- (bool): True if query contains ref bases surrounded by the flanks, False otherwise 
 	'''
+	# Extract the information in the alignment and flank_info objects
 	btop = alignment['btop']
 	ref_start = alignment['ref_start']
 	ref_stop = alignment['ref_stop']
-	left = flanks[0]
-	right = flanks[1]	
-
-	if ref_start < left or ref_stop > right: # Determine whether the variant even includes the variant
+	left = flank_info['left']
+	right = flank_info['right']	
+	length = flank_info['length']
+	# Determines whether the aligned subsequence of the reference even includes the variant
+	if ref_start > left or ref_stop < right: 
 		return False
 	else:
+		# Adjust the variant boundary indices to be compatible with the local alignment given by the BTOP
+		# string
 		left = left - ref_start
-		right = 
-
+		right = right - ref_start
+	# Convert the BTOP string into an alignment
 	delimited_btop = find_delimited_btop(btop)
-	ref, query = delimited_btop_to_alignment(delimited_btop)
-	alignment_left = find_left_alignment_flank(left,ref)
-	alignment_right = find_left_alignment_flank(right,ref[::-1])	
+	# Get the reference sequence alignment
+	ref = delimited_btop_to_alignment(delimited_btop)
+	# Translate the variant boundaries in the sequence into the boundaries for the alignment 
+	alignment_left = translate_var_boundary(left,ref)
+	alignment_right = translate_var_boundary(right,ref)
+	# Start and stop positions for the interval containing the variant in the alignment
 	start = alignment_left - 1
 	stop = len(ref) - alignment_right
+	# If any base in the interval containing the variant is not a match, then the query does not contain the
+	# variant
 	for i in range(start,stop):
-		query_base = query[i]
-		if query_base != MATCH:
+		ref_base = ref[i]
+		if ref_base != MATCH:
 			return False
 	return True 
 
 def unit_tests():
-	# Test data
+	# Set up test data
 	orig_ref = "AAAAGTTTTTTTTTTAAAA"
 	orig_query = "AAAACCAAAA"
 	btop = "4C-CG_10_4"
 	left = 16
 	right = 17
-	# Start testing each module individually
+	alignment = {'btop':btop,'ref_start':0,'ref_stop':len(orig_ref)}
+	flank_info = {'left':left,'right':right,'length':len(orig_ref)}
+
+	# Start unit tests
 	delimited_btop = find_delimited_btop(btop)
-	assert( delimited_btop == " 4 C- CG _10_ 4" )
+	assert(delimited_btop == " 4 C- CG _10_ 4")
 
-	ref, query = delimited_btop_to_alignment(delimited_btop)
-	assert( ( query == "====CC__________====") and (ref == "====-G__________====") )
+	ref = delimited_btop_to_alignment(delimited_btop)
+	assert(ref == "====-G__________====")
 
-	alignment_left = find_left_alignment_flank(left,ref)
-	alignment_right = find_left_alignment_flank(right,ref[::-1])
-	assert( alignment_left == left + 1 )
-	assert( alignment_right == right + 1 )
+	alignment_left = translate_var_boundary(left,ref)
+	alignment_right = translate_var_boundary(right,ref)
+	assert(alignment_left == left + 1)
+	assert(alignment_right == right + 1)
 
-	assert(query_contains_ref_base(btop,left,right))		
+	assert( query_contains_ref_base(alignment,flank_info) )
 
-	assert(not query_contains_ref_base(btop,5,6))		
+	alignment = {'btop':btop, 'ref_start':0, 'ref_stop':len(orig_ref)}
+	flank_info = {'left':5, 'right':6, 'length':len(orig_ref)}
+	assert( not query_contains_ref_base(alignment,flank_info) )
 
 	print("All unit tests passed!")
 
@@ -169,6 +201,10 @@ if __name__ == '__main__':
 	parser.add_argument('-i','--input_file',metavar='INPUT_FILE',type=str,help=
 		"""
 		Path to file containing Magic-BLAST tabulated output. If not set, will read through STDIN.	
+		""")
+	parser.add_argument('-f','--flank_info',metavar='FLANK_INFO',type=str,help=
+		"""
+		Path to the file containing information regarding the lengths of the variant flanking sequences
 		""")
 	parser.add_argument('-o','--output_path',metavar='OUTPUT_PATH',type=str,help=
 		"""
