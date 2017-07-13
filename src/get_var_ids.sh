7f107b06cf2caac79ad306c7ea8f556089fc718c
Sean La <seanla1996@gmail.com>
2017-07-13 15:56:59 -0700

    added script to retrieve variant IDs

---
+++ src/get_var_ids.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright: NCBI 2017
+# Author: Anmol Vohra
+
+if [ "$#" -ne 1 ]; then
+	echo "Description: given a disease phenotype, this script retrieves variants (currently only SNPs) related"
+	echo "to the phenotype."
+	BASENAME=`basename "$0"`
+	echo "Usage: ${BASENAME} [phenotype]"
+	exit 0
+fi
+
+PHENOTYPE=$1
+
+# Find variant IDs
+
+esearch -query ${PHENOTYPE} -ovarian_cancer -db pubmed | elink -target snp | esummary | xtract -pattern DocumentSummary -element SNP_ID
