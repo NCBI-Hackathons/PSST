3dc10d6503ca25b179ddab1d28dba9e45d4fb78c
Sean La <seanla1996@gmail.com>
2017-07-25 13:09:42 -0700

    updated psst to take as input threads and max num of procs

--- src/magicblast.sh
+++ src/magicblast.sh
@@ -2,22 +2,20 @@
 # Copyright: NCBI 2017
 # Author: Sean La
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 5 ]; then
 	echo "Description: Given a file containing SRA accessions and a BLAST database, this script runs Magic-BLAST" 
 	echo "             on each SRA dataset."
 	BASENAME=`basename "$0"`
-	echo "Usage: ${BASENAME} [SRA accessions file] [BLAST database name] [output directory]"
+	echo "Usage: ${BASENAME} [SRA accessions file] [BLAST DB name] [output dir] [threads] [max child procs]"
 	exit 0
 fi
-
-# Maximum number of concurrent Magic-BLAST runs at a time
-MAX_PROCS=10
-THREADS=8
 
 # Retrieve the command line arguments
 SRA=$1
 DB_NAME=$2
 OUTPUT_DIR=$3
+THREADS=$4
+MAX_PROCS=$5
 
 # This prevents ambiguous splicing from occuring in Magic-BLAST
 export MAPPER_NO_OVERLAPPED_HSP_MERGED=1
